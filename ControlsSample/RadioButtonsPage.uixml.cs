234ed943bb77af93b951be645aec566af29ab841
Yevgeni Zolotko <yevgeni.zolotko@alternetsoft.com>
2021-11-09 20:32:24 +0200

    Source code release for version 0.1.89-beta
    on-behalf-of: @alternetsoft <yevgeni.zolotko@alternetsoft.com>

---
+++ ControlsSample/RadioButtonsPage.uixml.cs
@@ -0,0 +1,24 @@
+﻿using Alternet.UI;
+using System;
+using System.Linq;
+
+namespace ControlsSample
+{
+    partial class RadioButtonsPage : Control
+    {
+        private readonly IPageSite site;
+
+        public RadioButtonsPage(IPageSite site)
+        {
+            InitializeComponent();
+
+            this.site = site;
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            var rb = (RadioButton)sender;
+            site.LogEvent("Radio button " + rb.Text + " IsChecked changed to " + rb.IsChecked);
+        }
+    }
+}
