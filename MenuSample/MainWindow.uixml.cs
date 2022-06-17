a379562b4fafd8ba4a588da1e8db9887e232e284
Yevgeni Zolotko <yevgeni.zolotko@alternetsoft.com>
2022-06-17 12:26:28 +0200

    Source code release for version 0.3.121-beta
    on-behalf-of: @alternetsoft <yevgeni.zolotko@alternetsoft.com>

---
+++ MenuSample/MainWindow.uixml.cs
@@ -0,0 +1,157 @@
+using Alternet.UI;
+using System;
+using System.Linq;
+
+namespace MenuSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SaveCommand = new Command(o => MessageBox.Show("Save"), o => saveEnabledMenuItem.Checked);
+            ExportToPngCommand = new Command(o => MessageBox.Show("Export to PNG"));
+            DataContext = this;
+
+            PlatformSpecificInitialize();
+            UpdateControls();
+        }
+
+        private void PlatformSpecificInitialize()
+        {
+#if NETCOREAPP
+            bool runningUnderMacOS = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+                System.Runtime.InteropServices.OSPlatform.OSX);
+#else
+            bool runningUnderMacOS = false;
+#endif
+
+            roleControlsPanel.Visible = runningUnderMacOS;
+        }
+
+        public Command SaveCommand { get; }
+        public Command ExportToPngCommand { get; }
+
+        private void OpenMenuItem_Click(object sender, EventArgs e) => MessageBox.Show("Open");
+
+        private void SaveEnabledMenuItem_Click(object sender, EventArgs e) => SaveCommand.RaiseCanExecuteChanged();
+
+        private void ExportToPdfMenuItem_Click(object sender, EventArgs e) => MessageBox.Show("Export to PDF");
+
+        private void ExportToPngMenuItem_Click(object sender, EventArgs e) => MessageBox.Show("Export to PNG");
+
+        private void OptionsMenuItem_Click(object sender, EventArgs e) => MessageBox.Show("Options");
+
+        private void ExitMenuItem_Click(object sender, EventArgs e) => Close();
+
+        private void AboutMenuItem_Click(object sender, EventArgs e) => MessageBox.Show("AlterNET UI Menu Sample Application.", "About");
+
+        MenuItem? dynamicItemsMenuItem;
+
+        MenuItem EnsureDynamicItemsMenuItem()
+        {
+            if (dynamicItemsMenuItem == null)
+            {
+                dynamicItemsMenuItem = new MenuItem("Dynamic Items");
+                Menu!.Items.Add(dynamicItemsMenuItem);
+            }
+
+            return dynamicItemsMenuItem;
+        }
+
+        private void AddDynamicMenuItemMenuItem_Click(object sender, EventArgs e)
+        {
+            var parent = EnsureDynamicItemsMenuItem();
+
+            int GetNextItemNumber()
+            {
+                if (parent.Items.Count == 0)
+                    return 1;
+
+                return (int)parent.Items.Last().Tag! + 1;
+            }
+
+            var number = GetNextItemNumber();
+            parent.Items.Add(new MenuItem("Item " + number, DynamicMenuItem_Click) { Tag = number });
+        }
+
+        private void DynamicMenuItem_Click(object? sender, EventArgs e)
+        {
+            var item = (MenuItem)sender!;
+            MessageBox.Show("Dynamic item clicked: " + item.Text);
+        }
+
+        private void RemoveLastDynamicMenuItemMenuItem_Click(object sender, EventArgs e)
+        {
+            var parent = dynamicItemsMenuItem;
+            if (parent == null)
+                return;
+
+            if (parent.Items.Count > 0)
+            {
+                parent.Items.RemoveAt(parent.Items.Count - 1);
+            }
+
+            if (parent.Items.Count == 0)
+            {
+                Menu!.Items.Remove(parent);
+                dynamicItemsMenuItem = null;
+            }
+        }
+
+        private void ToggleExitEnabledMenuItem_Click(object sender, EventArgs e)
+        {
+            exitMenuItem.Enabled = !exitMenuItem.Enabled;
+            UpdateControls();
+        }
+
+        private void UpdateControls()
+        {
+            toggleExitEnabledMenuItem.Text = (exitMenuItem.Enabled ? "Disable" : "Enable") + " Exit Menu Item";
+            toggleSeparatorMenuItem.Text = separatorMenuItem.Text == "-" ? "Make separator item to be normal" : "Make normal item to be separator";
+        }
+
+        private void ContinousScrollingMenuItem_Click(object sender, EventArgs e)
+        {
+            continousScrollingMenuItem.Checked = true;
+            pageScrollingMenuItem.Checked = false;
+        }
+
+        private void PageScrollingMenuItem_Click(object sender, EventArgs e)
+        {
+            continousScrollingMenuItem.Checked = false;
+            pageScrollingMenuItem.Checked = true;
+        }
+
+        private void GridMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Grid item is checked: " + ((MenuItem)sender).Checked);
+        }
+
+        private void ToggleSeparatorMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (separatorMenuItem.Text == "-")
+                separatorMenuItem.Text = "[Text instead of separator]";
+            else
+                separatorMenuItem.Text = "-";
+
+            UpdateControls();
+        }
+
+        private void SetAboutMenuItemRoleToNone_Click(object sender, System.EventArgs e)
+        {
+            aboutMenuItem.Role = MenuItemRoles.None;
+        }
+
+        private void SetOptionsMenuItemRoleToNone_Click(object sender, System.EventArgs e)
+        {
+            optionsMenuItem.Role = MenuItemRoles.None;
+        }
+
+        private void SetOptionsMenuItemRoleToPreferences_Click(object sender, System.EventArgs e)
+        {
+            optionsMenuItem.Role = MenuItemRoles.Preferences;
+        }
+    }
+}
---
+++ WindowPropertiesSample/MainWindow.uixml.cs
@@ -0,0 +1,353 @@
+using Alternet.Drawing;
+using Alternet.UI;
+using System;
+
+namespace WindowPropertiesSample
+{
+    public partial class MainWindow : Window
+    {
+        private TestWindow? testWindow;
+
+        private int lastEventNumber = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var value in Enum.GetValues(typeof(WindowState)))
+                stateComboBox.Items.Add(value!);
+
+            foreach (var value in Enum.GetValues(typeof(WindowStartLocation)))
+                startLocationComboBox.Items.Add(value!);
+            startLocationComboBox.SelectedItem = WindowStartLocation.Default;
+
+            UpdateControls();
+        }
+
+        private void CreateAndShowWindowButton_Click(object sender, EventArgs e)
+        {
+            CreateWindowAndSetProperties();
+
+            if (testWindow == null)
+                throw new InvalidOperationException();
+
+            testWindow.Show();
+
+            UpdateWindowState();
+            UpdateControls();
+        }
+
+        private void CreateAndShowModalWindowButton_Click(object sender, EventArgs e)
+        {
+            CreateWindowAndSetProperties();
+
+            if (testWindow == null)
+                throw new InvalidOperationException();
+
+            testWindow.ShowModal();
+
+            MessageBox.Show("ModalResult: " + testWindow.ModalResult);
+            testWindow.Dispose();
+            OnWindowClosed();
+        }
+
+        private void CreateWindowAndSetProperties()
+        {
+            testWindow = new TestWindow();
+
+            if (setOwnerCheckBox.IsChecked)
+                testWindow.Owner = this;
+
+            testWindow.BeginInit();
+
+            testWindow.ShowInTaskbar = showInTaskBarCheckBox.IsChecked;
+
+            testWindow.MinimizeEnabled = minimizeEnabledCheckBox.IsChecked;
+            testWindow.MaximizeEnabled = maximizeEnabledCheckBox.IsChecked;
+            testWindow.CloseEnabled = closeEnabledCheckBox.IsChecked;
+
+            testWindow.AlwaysOnTop = alwaysOnTopCheckBox.IsChecked;
+            testWindow.IsToolWindow = isToolWindowCheckBox.IsChecked;
+            testWindow.Resizable = resizableCheckBox.IsChecked;
+            testWindow.HasBorder = hasBorderCheckBox.IsChecked;
+            testWindow.HasTitleBar = hasTitleBarCheckBox.IsChecked;
+
+            testWindow.StartLocation = (WindowStartLocation)startLocationComboBox.SelectedItem!;
+            testWindow.Location = new Point();
+
+            testWindow.EndInit();
+
+            testWindow.Closed += TestWindow_Closed;
+            testWindow.Closing += TestWindow_Closing;
+            testWindow.Activated += TestWindow_Activated;
+            testWindow.Deactivated += TestWindow_Deactivated;
+            testWindow.StateChanged += TestWindow_StateChanged;
+            testWindow.SizeChanged += TestWindow_SizeChanged;
+            testWindow.LocationChanged += TestWindow_LocationChanged;
+        }
+
+        private void TestWindow_LocationChanged(object? sender, EventArgs e)
+        {
+            LogEvent("LocationChanged");
+            UpdateWindowBounds();
+        }
+
+        private void TestWindow_SizeChanged(object? sender, EventArgs e)
+        {
+            LogEvent("SizeChanged");
+            UpdateWindowBounds();
+        }
+
+        private void TestWindow_StateChanged(object? sender, EventArgs e)
+        {
+            LogEvent("StateChanged");
+            UpdateWindowState();
+        }
+
+        private void UpdateWindowState()
+        {
+            stateComboBox.SelectedItem = (testWindow ?? throw new Exception()).State;
+        }
+
+        private void UpdateWindowBounds()
+        {
+            currentBoundsLabel.Text = (testWindow ?? throw new Exception()).Bounds.ToString();
+        }
+
+        private void UpdateActiveWindowInfoLabel()
+        {
+            var title = ActiveWindow?.Title ?? "N/A";
+            activeWindowTitleLabel.Text = "Active window title: " + title;
+
+            if (testWindow != null)
+                isWindowActiveLabel.Text = "Test window active: " + (testWindow.IsActive ? "Yes" : "No");
+            else
+                isWindowActiveLabel.Text = string.Empty;
+        }
+
+        private void TestWindow_Deactivated(object? sender, EventArgs e)
+        {
+            LogEvent("Deactivated");
+            UpdateActiveWindowInfoLabel();
+        }
+
+        private void TestWindow_Activated(object? sender, EventArgs e)
+        {
+            LogEvent("Activated");
+            UpdateActiveWindowInfoLabel();
+        }
+
+        private void LogEvent(string message)
+        {
+            if (eventsListBox.IsDisposed)
+                return;
+
+            eventsListBox.Items.Add($"{lastEventNumber++}. {message}");
+            eventsListBox.SelectedIndex = eventsListBox.Items.Count - 1;
+        }
+
+        private void UpdateControls()
+        {
+            var haveTestWindow = testWindow != null;
+
+            hideWindowCheckBox.Enabled = haveTestWindow;
+            createAndShowWindowButton.Enabled = !haveTestWindow;
+            createAndShowModalWindowButton.Enabled = !haveTestWindow;
+            startLocationComboBox.Enabled = !haveTestWindow;
+
+            activateButton.Enabled = haveTestWindow;
+            addOwnedWindow.Enabled = haveTestWindow;
+            stateComboBox.Enabled = haveTestWindow;
+
+            setSizeButton.Enabled = haveTestWindow;
+            setClientSizeButton.Enabled = haveTestWindow;
+            setBoundsButton.Enabled = haveTestWindow;
+            increaseLocationButton.Enabled = haveTestWindow;
+            setMinMaxSizeButton.Enabled = haveTestWindow;
+            if (!haveTestWindow)
+                currentBoundsLabel.Text = string.Empty;
+
+            setIcon1Button.Enabled = haveTestWindow;
+            setIcon2Button.Enabled = haveTestWindow;
+
+            UpdateActiveWindowInfoLabel();
+        }
+
+        private void TestWindow_Closed(object? sender, WindowClosedEventArgs e)
+        {
+            LogEvent("Closed");
+
+            if (testWindow == null)
+                throw new InvalidOperationException();
+            if (testWindow.Modal)
+                return;
+
+            OnWindowClosed();
+        }
+
+        private void OnWindowClosed()
+        {
+            if (testWindow == null)
+                throw new InvalidOperationException();
+
+            testWindow.Activated -= TestWindow_Activated;
+            testWindow.Deactivated -= TestWindow_Deactivated;
+            testWindow.Closed -= TestWindow_Closed;
+            testWindow.Closing -= TestWindow_Closing;
+            testWindow.StateChanged -= TestWindow_StateChanged;
+            testWindow.SizeChanged -= TestWindow_SizeChanged;
+            testWindow.LocationChanged -= TestWindow_LocationChanged;
+
+            testWindow = null;
+
+            hideWindowCheckBox.IsChecked = false;
+
+            UpdateControls();
+        }
+
+        private void TestWindow_Closing(object? sender, WindowClosingEventArgs e)
+        {
+            LogEvent("Closing");
+            e.Cancel = cancelClosingCheckBox.IsChecked;
+        }
+
+        private void ShowInTaskBarCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.ShowInTaskbar = showInTaskBarCheckBox.IsChecked;
+        }
+
+        private void MinimizeEnabledCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.MinimizeEnabled = minimizeEnabledCheckBox.IsChecked;
+        }
+
+        private void MaximizeEnabledCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.MaximizeEnabled = maximizeEnabledCheckBox.IsChecked;
+        }
+
+        private void CloseEnabledCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.CloseEnabled = closeEnabledCheckBox.IsChecked;
+        }
+
+        private void AlwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.AlwaysOnTop = alwaysOnTopCheckBox.IsChecked;
+        }
+
+        private void IsToolWindowCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.IsToolWindow = isToolWindowCheckBox.IsChecked;
+        }
+
+        private void ResizableCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Resizable = resizableCheckBox.IsChecked;
+        }
+
+        private void HasBorderCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.HasBorder = hasBorderCheckBox.IsChecked;
+        }
+
+        private void HasTitleBarCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.HasTitleBar = hasTitleBarCheckBox.IsChecked;
+        }
+
+        private void ActivateButton_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Activate();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            UpdateActiveWindowInfoLabel();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            UpdateActiveWindowInfoLabel();
+        }
+
+        private void AddOwnedWindow_Click(object sender, EventArgs e)
+        {
+            if (testWindow == null)
+                return;
+
+            var ownedWindow = new OwnedWindow();
+            ownedWindow.Owner = testWindow;
+
+            ownedWindow.SetLabel("Owned Window #" + testWindow.OwnedWindows.Length);
+            ownedWindow.Show();
+        }
+
+        private void StateComboBox_SelectedItemChanged(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.State = (WindowState)(stateComboBox.SelectedItem ?? throw new Exception());
+        }
+
+        private void SetIcon1Button_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Icon = Icons.Icon1;
+        }
+
+        private void SetIcon2Button_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Icon = Icons.Icon2;
+        }
+
+        private void SetSizeButton_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Size = new Size(300, 300);
+        }
+
+        private void SetClientSizeButton_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.ClientSize = new Size(300, 300);
+        }
+
+        private void IncreaseLocationButton_Click(object sender, EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Location += new Size(10, 10);
+        }
+
+        private void SetBoundsButton_Click(object sender, System.EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Bounds = new Rect(0, 0, 400, 400);
+        }
+
+        private void SetMinMaxSizeButton_Click(object sender, System.EventArgs e)
+        {
+            if (testWindow != null)
+            {
+                testWindow.MinimumSize = new Size(100, 100);
+                testWindow.MaximumSize = new Size(300, 300);
+            }
+        }
+
+        private void HideWindowCheckBox_CheckedChanged(object sender, System.EventArgs e)
+        {
+            if (testWindow != null)
+                testWindow.Visible = !hideWindowCheckBox.IsChecked;
+        }
+    }
+}
